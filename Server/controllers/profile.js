8b0bed161b4c387664071a4aedfb5e9ecee1ddb6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-27 16:16:57 +0530

    Controller Created to Delete Profile

--- Server/controllers/profile.js
+++ Server/controllers/profile.js
@@ -37,3 +37,32 @@ exports.updateProfile = async (req, res) => {
         })
     }
 }
+
+exports.deleteAccount = async (req, res) => {
+    try {
+        const id = req.user.id;
+
+        const userDetails = await User.findById(id);
+        if (!userDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found"
+            })
+        }
+
+        await Profile.findByIdAndDelete({ _id: userDetails.additionalDetails });
+        await User.findByIdAndDelete({ _id: id });
+
+        return res.status(200).json({
+            success: true,
+            message: "User Deleted Successfully"
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "An Error Occurred While Deleting User",
+            error: error.message
+        })
+    }
+}
